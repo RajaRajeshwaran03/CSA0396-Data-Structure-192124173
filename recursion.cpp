d3f58bb042360dbee7be4e8baea9629569b90873
RajaRajeshwaran <114379354+RajaRajeshwaran03@users.noreply.github.com>
2023-01-11 11:06:50 +0530

    Add files via upload

---
+++ recursion.cpp
@@ -0,0 +1,17 @@
+//Rajarajeshwaran 192124173
+#include<stdio.h>
+long int multiplyNumbers(int n);
+int main() {
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+    printf("Factorial of %d = %ld", n, multiplyNumbers(n));
+    return 0;
+}
+
+long int multiplyNumbers(int n) {
+    if (n>=1)
+        return n*multiplyNumbers(n-1);
+    else
+        return 1;
+}
