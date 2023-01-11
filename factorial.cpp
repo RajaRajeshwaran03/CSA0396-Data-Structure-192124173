debbf782ab598ae77fa069622427fadfab98efb8
RajaRajeshwaran <114379354+RajaRajeshwaran03@users.noreply.github.com>
2023-01-11 10:44:13 +0530

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,21 @@
+//Rajarajeshwaran 192124173
+#include<stdio.h>
+int main()
+{
+	int i,fact=1,number;
+	printf("enter a number:");
+	scanf("%d",&number);
+	{
+	if(number<0){
+		printf("please enter a positive");
+		return 0;}
+	else
+	for(i=1;i<=number;i++)
+	{
+		fact=fact*i;
+	}
+		
+	}
+	printf("factorial of %d is: %d0",number,fact);
+	return 0;
+}
